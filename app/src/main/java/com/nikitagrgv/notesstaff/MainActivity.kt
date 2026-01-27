e9cd4243403441dbf773c15d9a8026c255f1d7c7
Nikita Grigorev <nikitagrgv@unigine.com>
2026-01-27 18:34:52 +0700

    w

--- app/src/main/java/com/nikitagrgv/notesstaff/MainActivity.kt
+++ app/src/main/java/com/nikitagrgv/notesstaff/MainActivity.kt
@@ -70,7 +70,7 @@ fun MusicStaffCanvas() {
             )
         }
 
-        val notePositions = listOf(0, 2, 4, 5, 6, -1, 10)
+        val notePositions = listOf(0, 12, -4, 5, 6, -1, 10)
 
         notePositions.forEachIndexed { index, pos ->
             val xOffset = 200f + (index * 120f)
