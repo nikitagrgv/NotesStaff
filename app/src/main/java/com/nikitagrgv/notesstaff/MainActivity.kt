61bbceed30f3ab0159c32c69e0f55885b8f168be
Nikita Grigorev <pettyaelse@gmail.com>
2026-01-28 00:14:38 +0700

    w

--- app/src/main/java/com/nikitagrgv/notesstaff/MainActivity.kt
+++ app/src/main/java/com/nikitagrgv/notesstaff/MainActivity.kt
@@ -51,12 +51,12 @@ fun Content(modifier: Modifier = Modifier) {
     }
 
     LaunchedEffect(Unit) {
-        val speedPxPerSecond = 130f
+        val speedPxPerSecond = 200f
         var previousTimeNanos = System.nanoTime()
         while (true) {
             withFrameNanos { frameTimeNanos ->
                 val timeDeltaSeconds = (frameTimeNanos - previousTimeNanos) / 1_000_000_000f
-                val speedMultiplier = if (renderer.scrollOffset > 600) 3f else 1f
+                val speedMultiplier = if (renderer.scrollOffset > 600) 5f else 1f
                 val speed = speedPxPerSecond * speedMultiplier
                 var nextOffset = renderer.scrollOffset - timeDeltaSeconds * speed
                 nextOffset = max(0f, nextOffset)
