49c48ff3b78c59bd136103fa72f18554c90b00a1
Nikita Grigorev <nikitagrgv@unigine.com>
2026-01-27 18:19:47 +0700

    w

--- app/src/main/java/com/nikitagrgv/notesstaff/MainActivity.kt
+++ app/src/main/java/com/nikitagrgv/notesstaff/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.nikitagrgv.notesstaff.ui.theme.NotesStaffTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,9 +24,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             NotesStaffTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MusicStaff(
+                    Content(
                         name = "Android",
-                        modifier = Modifier.padding(innerPadding).fillMaxSize()
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
                     )
                 }
             }
@@ -31,17 +37,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MusicStaff(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Content(name: String, modifier: Modifier = Modifier) {
+    Column(modifier = modifier.fillMaxSize())
+    {
+        Text(
+            text = "Hello $name!",
+            modifier = Modifier.padding(16.dp)
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        MusicStaffCanvas()
+    }
+}
+
+@Composable
+fun MusicStaffCanvas() {
+
 }
 
 @Preview(showBackground = true)
 @Composable
-fun MusicStaffPreview() {
+fun ContentPreview() {
     NotesStaffTheme {
-        MusicStaff("Android")
+        Content("Android")
     }
 }
