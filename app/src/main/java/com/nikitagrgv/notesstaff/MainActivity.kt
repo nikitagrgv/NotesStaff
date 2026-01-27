d88afc422845eefaa5196552027f5621cb3328ae
Nikita Grigorev <pettyaelse@gmail.com>
2026-01-28 00:15:33 +0700

    w

--- app/src/main/java/com/nikitagrgv/notesstaff/MainActivity.kt
+++ app/src/main/java/com/nikitagrgv/notesstaff/MainActivity.kt
@@ -51,7 +51,7 @@ fun Content(modifier: Modifier = Modifier) {
     }
 
     LaunchedEffect(Unit) {
-        val speedPxPerSecond = 200f
+        val speedPxPerSecond = 350f
         var previousTimeNanos = System.nanoTime()
         while (true) {
             withFrameNanos { frameTimeNanos ->
