2a75099ff74af3ac0995bf29194c26114c044538
Nikita Grigorev <pettyaelse@gmail.com>
2026-01-28 01:14:56 +0700

    w

--- app/src/main/java/com/nikitagrgv/notesstaff/MainActivity.kt
+++ app/src/main/java/com/nikitagrgv/notesstaff/MainActivity.kt
@@ -44,8 +44,8 @@ class MainActivity : ComponentActivity() {
 fun Content(modifier: Modifier = Modifier, notes: List<Int>) {
     var notePositions by remember { mutableStateOf(notes) }
     var numMistakes by remember { mutableIntStateOf(0) }
-    val minNotePosition = -6
-    val maxNotePosition = 8 + 6
+    val minNotePosition = -8
+    val maxNotePosition = 8 + 8
 
     while (notePositions.count() < 8) {
         notePositions += (minNotePosition..maxNotePosition).random()
